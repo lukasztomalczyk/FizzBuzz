b18ae53e54d2f3239ab3d2adf682e78d6f8cac58
Łukasz Tomalczyk <lukasz.tomalczyk@onet.pl>
2017-08-24 20:42:24 +0200

    test

---
+++ FizzBuzzTest/FizzBuzzTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FizzBuzzTest
+{
+    [TestFixture]
+    public class FizzBuzzTest
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
